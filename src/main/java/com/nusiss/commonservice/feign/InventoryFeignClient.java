7c4df6f686ff7857897230f2b567caf25bb144f5
SONG YINRUI <78004370+RickSong218@users.noreply.github.com>
2025-05-26 22:03:27 +0800

    Update the configuration information of the common service.

--- src/main/java/com/nusiss/commonservice/feign/InventoryFeignClient.java
+++ src/main/java/com/nusiss/commonservice/feign/InventoryFeignClient.java
@@ -1,6 +1,7 @@
 package com.nusiss.commonservice.feign;
 
 import com.nusiss.commonservice.config.ApiResponse;
+import com.nusiss.commonservice.entity.InventoryChangeRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
