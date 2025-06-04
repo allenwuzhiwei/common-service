5833ea3a3ac0c7eae0fb54d7846cc7e49f37df73
SONG YINRUI <78004370+RickSong218@users.noreply.github.com>
2025-06-04 16:39:49 +0800

    Update the OrderFeignClient to invoke the Order service

---
+++ src/main/java/com/nusiss/commonservice/feign/OrderFeignClient.java
@@ -0,0 +1,20 @@
+package com.nusiss.commonservice.feign;
+
+import com.nusiss.commonservice.config.ApiResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "order-service")
+public interface OrderFeignClient {
+
+    /*
+     获取指定用户下单过的所有商品 ID 列表
+     */
+    @GetMapping("/order-items/user/{userId}/product-ids")
+    ApiResponse<List<Long>> getProductIdsByUserId(@PathVariable("userId") Long userId);
+
+    // 未来可添加更多接口
+}
